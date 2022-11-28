87a984c55c0ec4b16a09c45a59b8fd8b1461c044
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-11-28 13:14:54 -0500

    commit

---
+++ vite-project/js/data.js
@@ -0,0 +1,61 @@
+const data = [
+    {
+        name: "Geforce RTX 3050",
+        brand: "NVIDIA",
+        brandSpecific: {
+            architecture: "Ampere",
+            tiSuperVariant: "none",
+        },
+        mobile: true,
+        memoryGB: 4,
+        rayTracing: true,
+        imgLink: "https://www.pcgameshardware.de/screenshots/610x305/2022/01/NVIDIA-RTX3050-GA106-GPU-1-pcgh_b2article_artwork.jpg",
+    },
+    {
+        name: "Radeon RX 6650XT",
+        brand: "AMD",
+        brandSpecific: {
+            architecture: "RDNA 2",
+            xtxVariant: "xt",
+        },
+        mobile: false,
+        memoryGB: 8,
+        rayTracing: true,
+        imgLink: "https://m.media-amazon.com/images/I/61y3nA6JgdL.jpg",
+    },
+    {
+        name: "Geforce GTX 1650 SUPER",
+        brand: "NVIDIA",
+        brandSpecific: {
+            architecture: "Turing",
+            tiSuperVariant: "super",
+        },
+        mobile: true,
+        memoryGB: 4,
+        rayTracing: true,
+        imgLink: "https://m.media-amazon.com/images/I/81-lyRrRDqL.jpg",
+    },
+    {
+        name: "Radeon RX 570",
+        brand: "AMD",
+        brandSpecific: {
+            architecture: "GCN 4",
+            xtxVariant: "none",
+        },
+        mobile: false,
+        memoryGB: 4,
+        rayTracing: true,
+        imgLink: "https://m.media-amazon.com/images/I/61y3nA6JgdL.jpg",
+    },
+    {
+        name: "Radeon RX 5700XT",
+        brand: "AMD",
+        brandSpecific: {
+            architecture: "RNDA 1",
+            xtxVariant: "xt",
+        },
+        mobile: false,
+        memoryGB: 8,
+        rayTracing: true,
+        imgLink: "https://m.media-amazon.com/images/I/61y3nA6JgdL.jpg",
+    }
