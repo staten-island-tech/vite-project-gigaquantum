253362424f76c3773d61a242ca078cc0a08d009e
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-12-12 13:13:05 -0500

    js done

---
+++ vite-project/js/functions.js
@@ -0,0 +1,153 @@
+import { gpuData } from "./gpuData.js";
+
+const products = {
+  addCard: function (
+    imageLink,
+    productName,
+    productBrand,
+    productDesc,
+    productPrice
+  ) {
+    document.getElementById("card-bin").insertAdjacentHTML(
+      "beforeend",
+      `<div class="item-card">
+    <img class="card-img" src="${imageLink}" alt="${productBrand} ${productName}">
+    <h2 class="card-title">${productBrand} ${productName}</h2>
+    <p class="card-desc">${productDesc}</p>
+    <p class="card-price">$${productPrice}</p>
+    </div>`
+    );
+  },
+  addAllCards: function () {
+    gpuData.forEach((gpu) =>
+      products.addCard(gpu.imgLink, gpu.name, gpu.brand, gpu.desc, gpu.price)
+    );
+  },
+  removeAllCards: function (selector) {
+    document.querySelectorAll(selector).forEach((item) => item.remove());
+  },
+  resetAll: function () {
+    products.removeAllCards(".item-card");
+    products.addAllCards();
+    buttons.resetButtons(".filter-btn");
+    console.log("reset");
+  },
+};
+
+const filterFunctions = {
+  filterByMatch: function (filterType, targetValue) {
+    gpuData
+      .filter((element) => element[filterType] == targetValue)
+      .forEach((filteredGPU) => {
+        products.addCard(
+          filteredGPU.imgLink,
+          filteredGPU.name,
+          filteredGPU.brand,
+          filteredGPU.desc,
+          filteredGPU.price
+        );
+        console.log(filteredGPU);
+      });
+  },
+
+  compareGreater: function (filterType, targetValue) {
+    gpuData
+      .filter((gpu) => gpu[filterType] > targetValue)
+      .forEach((filteredGPU) => {
+        products.addCard(
+          filteredGPU.imgLink,
+          filteredGPU.name,
+          filteredGPU.brand,
+          filteredGPU.desc,
+          filteredGPU.price
+        );
+      });
+  },
+
+  compareLess: function (filterType, targetValue) {
+    gpuData
+      .filter((gpu) => gpu[filterType] < targetValue)
+      .forEach((filteredGPU) => {
+        products.addCard(
+          filteredGPU.imgLink,
+          filteredGPU.name,
+          filteredGPU.brand,
+          filteredGPU.desc,
+          filteredGPU.price
+        );
+      });
+  },
+
+  filterByCompare: function (filterType, targetValue, greaterOrLess) {
+    if (greaterOrLess == "greater") {
+      filterFunctions.compareGreater(filterType, targetValue);
+    } else {
+      filterFunctions.compareLess(filterType, targetValue);
+    }
+  },
+
+  displayFilteredItems: function (
+    filterType,
+    targetValue,
+    filterMethod,
+    greaterOrLess
+  ) {
+    if (filterMethod == "match") {
+      filterFunctions.filterByMatch(filterType, targetValue);
+    }
+    if (filterMethod == "compare") {
+      filterFunctions.filterByCompare(filterType, targetValue, greaterOrLess);
+    }
+  },
+};
+
+const buttons = {
+  resetButtons: function (selector) {
+    document.querySelectorAll(selector).forEach((btn) => {
+      btn.style.color = "black";
+      btn.style.backgroundColor = "white";
+    });
+  },
+
+  highlightButton: function (btnID) {
+    document.getElementById(btnID).style.color = "white";
+    document.getElementById(btnID).style.backgroundColor = "black";
+  },
+
+  activateFilter: function (
+    btnID,
+    filterType,
+    targetValue,
+    filterMethod,
+    greaterOrLess
+  ) {
+    buttons.resetButtons(".filter-btn");
+    buttons.highlightButton(btnID);
+    products.removeAllCards(".item-card");
+    filterFunctions.displayFilteredItems(
+      filterType,
+      targetValue,
+      filterMethod,
+      greaterOrLess
+    );
+  },
+  filterEventListener: function (
+    btnID,
+    filterType,
+    targetValue,
+    filterMethod,
+    greaterOrLess
+  ) {
+    document.getElementById(btnID).addEventListener("click", function () {
+      buttons.activateFilter(
+        document.getElementById(btnID).id,
+        filterType,
+        targetValue,
+        filterMethod,
+        greaterOrLess
+      );
+    });
+  },
+};
+
+export { products, filterFunctions, buttons };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,205 +1,24 @@
 import "../styles/style.css";
-import { gpuData } from "./gpuData.js";
-
-const products = {
-  addCard: function (
-    imageLink,
-    productName,
-    productBrand,
-    productDesc,
-    productPrice
-  ) {
-    document.getElementById("card-bin").insertAdjacentHTML(
-      "beforeend",
-      `<div class="item-card">
-  <img class="card-img" src="${imageLink}" alt="${productBrand} ${productName}">
-  <h2 class="card-title">${productBrand} ${productName}</h2>
-  <p class="card-desc">${productDesc}</p>
-  <p class="card-price">$${productPrice}</p>
-  </div>`
-    );
-  },
-  addAllCards: function () {
-    gpuData.forEach((gpu) =>
-      products.addCard(gpu.imgLink, gpu.name, gpu.brand, gpu.desc, gpu.price)
-    );
-  },
-  removeAllCards: function (selector) {
-    document.querySelectorAll(selector).forEach((item) => item.remove());
-  },
-};
-
-function resetButtons(selector) {
-  document.querySelectorAll(selector).forEach((btn) => {
-    btn.style.color = "black";
-    btn.style.backgroundColor = "white";
-  });
-}
-
-function filterByMatch(filterType, targetValue) {
-  gpuData
-    .filter((element) => element[filterType] == targetValue)
-    .forEach((filteredGPU) => {
-      products.addCard(
-        filteredGPU.imgLink,
-        filteredGPU.name,
-        filteredGPU.brand,
-        filteredGPU.desc,
-        filteredGPU.price
-      );
-      console.log(filteredGPU);
-    });
-}
-
-function compareGreater(filterType, targetValue) {
-  gpuData
-    .filter((gpu) => gpu[filterType] > targetValue)
-    .forEach((filteredGPU) => {
-      products.addCard(
-        filteredGPU.imgLink,
-        filteredGPU.name,
-        filteredGPU.brand,
-        filteredGPU.desc,
-        filteredGPU.price
-      );
-    });
-}
-
-function compareLess(filterType, targetValue) {
-  gpuData
-    .filter((gpu) => gpu[filterType] < targetValue)
-    .forEach((filteredGPU) => {
-      products.addCard(
-        filteredGPU.imgLink,
-        filteredGPU.name,
-        filteredGPU.brand,
-        filteredGPU.desc,
-        filteredGPU.price
-      );
-    });
-}
-
-function filterByCompare(filterType, targetValue, greaterOrLess) {
-  if (greaterOrLess == "greater") {
-    compareGreater(filterType, targetValue);
-  } else {
-    compareLess(filterType, targetValue);
-  }
-}
-
-function displayFilteredItems(
-  filterType,
-  targetValue,
-  filterMethod,
-  greaterOrLess
-) {
-  if (filterMethod == "match") {
-    filterByMatch(filterType, targetValue);
-  }
-  if (filterMethod == "compare") {
-    filterByCompare(filterType, targetValue, greaterOrLess);
-  }
-}
-
-function highlightButton(btnID) {
-  document.getElementById(btnID).style.color = "white";
-  document.getElementById(btnID).style.backgroundColor = "black";
-}
-
-function activateFilter(
-  btnID,
-  filterType,
-  targetValue,
-  filterMethod,
-  greaterOrLess
-) {
-  resetButtons(".filter-btn");
-  highlightButton(btnID);
-  products.removeAllCards(".item-card");
-  displayFilteredItems(filterType, targetValue, filterMethod, greaterOrLess);
-}
-
-function resetAll() {
-  products.removeAllCards(".item-card");
-  products.addAllCards();
-  resetButtons(".filter-btn");
-  console.log("reset");
-}
+import { products, filterFunctions, buttons } from "./functions.js";
 
 // Initially add the cards to the html
 products.addAllCards();
 
 // Adding event listeners for each filter button
-document.getElementById("nvidia").addEventListener("click", function () {
-  activateFilter(
-    document.getElementById("nvidia").id,
-    "brand",
-    "NVIDIA",
-    "match"
-  );
-});
+buttons.filterEventListener("nvidia", "brand", "NVIDIA", "match");
+buttons.filterEventListener("amd", "brand", "AMD", "match");
+buttons.filterEventListener("sale", "sale", true, "match");
+buttons.filterEventListener("stock", "inStock", true, "match");
+buttons.filterEventListener("ray-tracing", "rayTracing", true, "match");
+buttons.filterEventListener("high-ram", "memoryGB", 6, "compare", "greater");
+buttons.filterEventListener("low-ram", "memoryGB", 6, "compare", "less");
+buttons.filterEventListener("amd", "brand", "AMD", "match");
+buttons.filterEventListener("amd", "brand", "AMD", "match");
+buttons.filterEventListener("amd", "brand", "AMD", "match");
+buttons.filterEventListener("amd", "brand", "AMD", "match");
+buttons.filterEventListener("amd", "brand", "AMD", "match");
 
-function filterEventListener(
-  btnID,
-  filterType,
-  targetValue,
-  filterMethod,
-  greaterOrLess
-) {
-  document.getElementById(btnID).addEventListener("click", function () {
-    activateFilter(
-      document.getElementById(btnID).id,
-      filterType,
-      targetValue,
-      filterMethod,
-      greaterOrLess
-    );
-  });
-}
-
-filterEventListener("amd", "brand", "AMD", "match");
-
-/* document.getElementById("amd").addEventListener("click", function () {
-  activateFilter(document.getElementById("amd").id, "brand", "AMD", "match");
-}); */
-
-document.getElementById("sale").addEventListener("click", function () {
-  activateFilter(document.getElementById("sale").id, "sale", true, "match");
-});
-
-document.getElementById("stock").addEventListener("click", function () {
-  activateFilter(document.getElementById("stock").id, "inStock", true, "match");
-});
-
-document.getElementById("ray-tracing").addEventListener("click", function () {
-  activateFilter(
-    document.getElementById("ray-tracing").id,
-    "rayTracing",
-    true,
-    "match"
-  );
-});
-
-document.getElementById("high-ram").addEventListener("click", function () {
-  activateFilter(
-    document.getElementById("high-ram").id,
-    "memoryGB",
-    6,
-    "compare",
-    "greater"
-  );
-});
-
-document.getElementById("low-ram").addEventListener("click", function () {
-  activateFilter(
-    document.getElementById("low-ram").id,
-    "memoryGB",
-    6,
-    "compare",
-    "less"
-  );
-});
-
+// Adding event listener for reset button
 document.getElementById("reset").addEventListener("click", function () {
-  resetAll();
+  products.resetAll();
 });
