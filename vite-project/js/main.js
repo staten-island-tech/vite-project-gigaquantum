37d722dca9a02fb5dea3222edcf58d79907dc7f3
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2022-12-13 13:14:22 -0500

    commit

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -4,6 +4,9 @@ import { products, filterFunctions, buttons } from "./functions.js";
 // Initially add the cards to the html
 products.addAllCards();
 
+//Initially set theme
+let theme = "light";
+
 // Adding event listeners for each filter button
 buttons.filterEventListener("nvidia", "brand", "NVIDIA", "match");
 buttons.filterEventListener("amd", "brand", "AMD", "match");
@@ -21,4 +24,11 @@ buttons.filterEventListener("amd", "brand", "AMD", "match");
 // Adding event listener for reset button
 document.getElementById("reset").addEventListener("click", function () {
   products.resetAll();
+});
+
+// Adding event listener for theme button
+document.getElementById("theme-btn").addEventListener("click", function () {
+  if (9 == 9) {
+    return;
+  }
 });
